3ba250f94023a05e0143f49ad50a4ef99fdec2ba
Korede <oyeyemikorede5@gmail.com>
2025-04-21 11:10:04 +0100

    'update'

--- components/allNews/index.tsx
+++ components/allNews/index.tsx
@@ -4,7 +4,6 @@ import { fetchPostsByCategory } from "@/lib/firestore";
 import styles from "./styles.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/header";
 
 interface FirestoreTimestamp {
   toDate(): Date;
