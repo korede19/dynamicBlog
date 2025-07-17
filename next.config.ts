0b6b7e812ea449076bd647429a0746e89a862cd0
Korede <oyeyemikorede5@gmail.com>
2025-07-17 21:43:40 +0100

    'update'

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
+import path from 'path';
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
