6ea530b4fc344a993c7e7db0e548b31d3b76aed3
Korede <oyeyemikorede5@gmail.com>
2025-07-26 10:21:17 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,19 +30,6 @@ export default function RootLayout({
     <html lang="en" className={`${encodeSans.variable} ${lato.variable}`}>
       <meta name="monetag" content="7bbdcbb2baafcde4d49d37f6d395afd6"></meta>
       <body className={encodeSans.className}>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RV6NGH4RBE"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-RV6NGH4RBE');
-          `}
-        </Script>
-
         {/* App UI */}
         <MegaMenu />
         <main>
@@ -58,23 +45,7 @@ export default function RootLayout({
           strategy="beforeInteractive"
         />
         <script type='text/javascript' src='//pl27242710.profitableratecpm.com/66/50/8a/66508a9c4445f297d8c780783f876255.js'></script>
-        <script async={true} data-cfasync="false" src="//pl27261617.profitableratecpm.com/cf13a514ad8f0327c56109e0993b42c4/invoke.js"></script>
-<div id="container-cf13a514ad8f0327c56109e0993b42c4"></div>
-<Script id="ad-options" strategy="beforeInteractive">
-  {`
-    atOptions = {
-      key: '513143e2bd3c79f09c6bc05d8edf528b',
-      format: 'iframe',
-      height: 250,
-      width: 300,
-      params: {}
-    };
-  `}
-</Script>
-<Script
-  src="//www.highperformanceformat.com/513143e2bd3c79f09c6bc05d8edf528b/invoke.js"
-  strategy="beforeInteractive"
-/>
+        <script data-cfasync="false" type='text/javascript' src='//pl27242710.profitableratecpm.com/66/50/8a/66508a9c4445f297d8c780783f876255.js'></script>
       </body>
     </html>
   );
