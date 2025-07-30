9be51bb153971024b962f437c8b08fdcb3bdceae
Korede <oyeyemikorede5@gmail.com>
2025-07-30 09:00:07 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,8 @@ const LandingPage = () => {
           <PostCard categoryId="category1" categoryName="Fitness" />
           <PostCard categoryId="category2" categoryName="Nutrition" />
         </div>
+        <script async data-cfasync="false" src="//pl27261617.profitableratecpm.com/cf13a514ad8f0327c56109e0993b42c4/invoke.js"></script>
+<div id="container-cf13a514ad8f0327c56109e0993b42c4"></div>
       </div>
       <DiscoverCategories />
       <AllPosts
