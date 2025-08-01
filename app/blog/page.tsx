684bacaf51385da9e5c6de1e9c5ab2bdf0e0d151
Korede <oyeyemikorede5@gmail.com>
2025-08-01 23:15:28 +0100

    'update'

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,6 @@
 import AllPostsPage from "@/components/allPostPage";
 import Footer from "@/components/footer";
 import React from "react";
-import styles from "./styles.module.css";
 
 const AllPagePosts = () => {
   const categoryNames = {
@@ -12,14 +11,6 @@ const AllPagePosts = () => {
 
   return (
     <div>
-       <div className={styles.newCol}>
-       {/* <script
-          async
-          data-cfasync="false"
-          src="//pl27261617.profitableratecpm.com/cf13a514ad8f0327c56109e0993b42c4/invoke.js"
-        ></script>
-        <div id="container-cf13a514ad8f0327c56109e0993b42c4"></div> */}
-        </div>
       <AllPostsPage categoryNames={categoryNames} />
       <Footer />
     </div>
