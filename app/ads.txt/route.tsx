a7dfb254f081219b43247a6afacb72f28e54069e
Korede <oyeyemikorede5@gmail.com>
2025-08-02 21:09:08 +0100

    update

---
+++ app/ads.txt/route.tsx
@@ -0,0 +1,11 @@
+export async function GET() {
+  const adsContent = `google.com, ca-pub-6031925946912275, DIRECT, f08c47fec0942fa0`;
+  
+  return new Response(adsContent, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=86400', // Cache for 24 hours
+    },
+  });
+}
